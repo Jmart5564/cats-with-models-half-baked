02a20a2c07b9a874d566fa6fc1401407fe6aab69
Jmart5564 <Jmart5564@gmail.com>
2022-07-27 12:14:27 -0700

    added all the cats

--- sql/setup.sql
+++ sql/setup.sql
@@ -23,11 +23,64 @@ INSERT INTO cats (name, type, ) VALUES
 ),
 
 (
-    '2',
     'Garfield',
     'Orange Tabby',
     'https://static.wikia.nocookie.net/garfield/images/9/9f/GarfieldCharacter.jpg',
     1978,
     7,
-    false,
-)
+    false
+),
+
+(
+    'Duchess',
+    'Angora',
+    'https://static.wikia.nocookie.net/disney/images/e/eb/Profile_-_Duchess.jpeg',
+    1970,
+    9,
+    false
+),
+
+(
+    'Stimpy',
+    'Manx',
+    'https://static.wikia.nocookie.net/renandstimpy/images/c/c1/Ren-stimpy-25-anniversar-hp2.png',
+    1990,
+    1,
+    true
+),
+
+(
+    'Sylvester',
+    'Tuxedo',
+    'https://static.wikia.nocookie.net/charactercommunity/images/7/73/SylvesterDance.png',
+    1945,
+    1,
+    true
+),
+
+(
+    'Tigger',
+    'Tiger',
+    'https://www.pinclipart.com/picdir/big/150-1504133_tigger-tigger-cartoon-me-clipart-png-image-download.png',
+    1928,
+    8,
+    false
+),
+
+(
+    'Hello Kitty',
+    'Angora',
+    'https://cdn.shopify.com/s/files/1/0054/4371/5170/products/FiGPiN_360HelloKittySANRIOPIN.png?v=1627413934',
+    1974,
+    9,
+    false
+),
+
+(
+    'Hobbes',
+    'Tiger',
+    'https://sketchok.com/images/articles/01-cartoons/000-va/102/10.jpg',
+    1985,
+    6,
+    true
+);
